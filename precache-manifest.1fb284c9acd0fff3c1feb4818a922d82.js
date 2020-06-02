f50fb19bb7c20b54708064808f7038283e7ed8d6
pashadidychenko <pasha.didychenko@gmail.com>
2020-06-02 22:39:25 +0300

    Updates

---
+++ precache-manifest.1fb284c9acd0fff3c1feb4818a922d82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5fcceb0f788520cf1da8ab45032054d",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "aab507e70373091f587b",
+    "url": "/goit-react-hw-01-components/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "57cc350bedf9d15a6de4",
+    "url": "/goit-react-hw-01-components/static/js/2.835e3c52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.835e3c52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aab507e70373091f587b",
+    "url": "/goit-react-hw-01-components/static/js/main.81d1d8ad.chunk.js"
+  },
+  {
+    "revision": "cf184cad0926235fce4a",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.f8c9733c.js"
+  }
+]);
